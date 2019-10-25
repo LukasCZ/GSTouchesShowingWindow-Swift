04e7b3e92586d516755b7be24671301ca274171c
Mike Monaghan <mike@ignitedsoftware.com>
2019-10-25 15:29:49 +1100

    Made appearance settings public. Provide ability to enable/disable touch appearance display. Disabled approach minimises resource usage to can be left embedded in codebase.

--- GSTouchesShowingWindow-Swift/Classes/GSTouchesShowingWindow.swift
+++ GSTouchesShowingWindow-Swift/Classes/GSTouchesShowingWindow.swift
@@ -8,27 +8,49 @@
 
 import UIKit
 
+public struct TouchAppearance {
+    var shortTapTresholdDuration = 0.11
+    var shortTapInitialCircleRadius : CGFloat = 22.0
+    var shortTapFinalCircleRadius : CGFloat = 57.0
+    var touchColor = UIColor(red: 0.0/255, green: 135.0/255, blue: 244.0/255, alpha: 0.8)
+}
+
 public class GSTouchesShowingWindow: UIWindow {
-    let controller = GSTouchesShowingController()
+  
+  var showTouchesEnabled = false
+  lazy var controller = GSTouchesShowingController()    // lets not consume any resources if not required
+  
+  var touchAppearance:TouchAppearance {
+    get {
+      return controller.appearance
+    }
+    set(appearance) {
+      controller.appearance = appearance
+    }
+  }
     
     override public func sendEvent(_ event: UIEvent) {
-        let touches = event.allTouches
-        
-        for touch in touches! {
-            switch touch.phase {
-            case .began:
-                self.controller.touchBegan(touch, view: self)
-                
-            case .moved:
-                self.controller.touchMoved(touch, view: self)
-                
-            case .ended, .cancelled:
-                self.controller.touchEnded(touch, view: self)
-            
-            default:
-                break
-            }
-        }
-        super.sendEvent(event)
+      
+      // if not showing touches, default to super behaviour
+      guard self.showTouchesEnabled else {
+        return super.sendEvent(event)
+      }
+      let touches = event.allTouches
+      for touch in touches! {
+          switch touch.phase {
+          case .began:
+              self.controller.touchBegan(touch, view: self)
+              
+          case .moved:
+              self.controller.touchMoved(touch, view: self)
+              
+          case .ended, .cancelled:
+              self.controller.touchEnded(touch, view: self)
+          
+          default:
+              break
+          }
+      }
+      super.sendEvent(event)
     }
 }
